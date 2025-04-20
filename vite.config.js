53c20ae12cda73ec30d720768e860dd816693a1a
unknown <goelshivam931@gmail.com>
2025-04-20 20:21:20 +0530

    correcting path again

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/React-course-social-media/", // use your repo name here
   plugins: [react()],
 });
